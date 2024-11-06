645e8f9c2fc97ac0d06318dfb22d8d924f6c084c
Borys-dotcom <184245222+Borys-dotcom@users.noreply.github.com>
2024-11-06 16:44:29 +0100

    new function

--- js/script.js
+++ js/script.js
@@ -1,14 +1,20 @@
 let inputArray = [0,1,2,3,4,5];
 
-function sumSquares(array){
-    let result = 0;
+// function sumSquares(array){
+//     let result = 0;
 
-    for(i=0; i<array.length; i++){
-        result += Math.pow(array[i], 2);
-    }
+//     for(i=0; i<array.length; i++){
+//         result += Math.pow(array[i], 2);
+//     }
 
-    console.log(result);
+//     console.log(result);
 
-}
+// }
 
-sumSquares(inputArray);
+// sumSquares(inputArray);
+
+sumOfSquares = inputArray.reduce((result, record) => {
+    return result + Math.pow(record,2);
+}, 0);
+
+console.log(sumOfSquares);
